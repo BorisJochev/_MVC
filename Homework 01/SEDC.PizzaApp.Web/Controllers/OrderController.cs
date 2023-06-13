76917c9502dd6680c9fc79b8cf08b8913375d950
BorisJochev <119973977+BorisJochev@users.noreply.github.com>
2023-06-13 22:10:46 +0200

    Homework 01

---
+++ Homework 01/SEDC.PizzaApp.Web/Controllers/OrderController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Web.Models.Domain;
+
+namespace SEDC.PizzaApp.Web.Controllers
+{
+    
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+      
+
+        public IActionResult Empty()
+        {
+            return new EmptyResult();
+        }
+
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("Index","Home");
+        }
+
+        public IActionResult Details(int? id) 
+        {
+           if (id == null)
+            {
+                return RedirectToAction("Empty");
+            }
+
+           return View("Index");
+        }
+
+        public IActionResult JsonData() 
+        {
+            Pizza pizza = new Pizza()
+            {
+                Id = 1,
+                Name = "PizzaTest",
+                Price = 100,
+                IsOnPromotion = false,
+
+
+            };
+            return new JsonResult(pizza);
+        }
+    }
+}
